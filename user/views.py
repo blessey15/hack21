21bd0c4bf5e817e556968d515ac39c67b7641df1
Melvin-Thomas-Dev <57299818+Melvin-Thomas-Dev@users.noreply.github.com>
2020-05-28 05:59:35 +0530

    user creation done

--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login 
 
 from .forms import RegisterForm
 
@@ -7,13 +8,24 @@ from .forms import RegisterForm
 def home(request):
     return render(request, 'home.html')
 
+# def signup(request):
+#     if request.method == 'POST':
+#         form = SignUpForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             auth_login(request, user)
+#             return redirect('home')
+#     else:
+#         form = SignUpForm()
+#     return render(request, 'signup.html', {'form': form})
+
     
 def register_view(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home/') 
+            return redirect('home') 
     else:
         form = RegisterForm()
 
