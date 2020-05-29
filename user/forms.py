bc210a6dae3264cec92e7a3725bba18ec706af3d
Melvin-Thomas-Dev <57299818+Melvin-Thomas-Dev@users.noreply.github.com>
2020-05-29 06:01:23 +0530

    oru cheriya form modification

--- user/forms.py
+++ user/forms.py
@@ -4,28 +4,6 @@ from django.contrib.auth.forms import UserCreationForm
 
 from .models import CustomUser
 
-# class SignUpForm(UserCreationForm):
-# 	email=forms.CharField(max_length=254,required=True, widget=forms.EmailInput())
-# 	class Meta:
-# 		model=CustomUser
-# 		fields=('username', 'email','password1','password2')
-
-
-# class RegisterForm(UserCreationForm):
-#     email = forms.EmailField(required=True)
-
-#     class Meta:
-#         model = CustomUser
-#         fields = ('email', 'password1', 'password2')
-
-#     def save(self, commit=True):
-#         user = super(RegisterForm, self).save(commit=False)
-#         def save(self, commit=True):
-#             user = super(RegisterForm, self).save(commit=False)
-#             user.email = self.cleaned_data['email']
-#             if commit:
-#                 user.save()
-#             return user
 
 
 class RegisterForm(forms.ModelForm):
