b379efaf398a52131ba15c8a7bdf69abfbcbb0d8
Melvin-Thomas-Dev <57299818+Melvin-Thomas-Dev@users.noreply.github.com>
2020-06-01 23:22:56 +0530

    enthokkeyo changes

--- user/forms.py
+++ user/forms.py
@@ -4,55 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 from .models import CustomUser
 
-# class RegisterForm(UserCreationForm):
-#     # email = forms.EmailField(required=True)
-#     password = forms.CharField(widget=forms.PasswordInput)
-#     password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
-
-
-#     class Meta:
-#         model = CustomUser
-#         fields = ('email',)#, 'password1', 'password2')
-
-#     def save(self, commit=True):
-#         user = super(RegisterForm, self).save(commit=False)
-#         def save(self, commit=True):
-#             user = super(RegisterForm, self).save(commit=False)
-#             user.email = self.cleaned_data['email']
-#             if commit:
-#                 user.save()
-#             return user
-
-
-class RegisterForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
-
-    class Meta:
-        model = CustomUser
-        fields = ('email',)
-
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        qs = CustomUser.objects.filter(email=email)
-        if qs.exists():
-            raise forms.ValidationError("email is taken")
-        return email
-
-    def clean_password2(self):
-        # Check that the two password entries match
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords! don't match")
-        return password2
-    def save(self, commit=True):
-        # Save the provided password in hashed format
-        user = super(RegisterForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password2"])
-        if commit:
-            user.save()
-        return user
+# class RegisterForm
 
 
 class CustomUserAdminCreationForm(forms.ModelForm):
@@ -101,3 +53,54 @@ class CustomUserAdminChangeForm(forms.ModelForm):
         # This is done here, rather than on the field, because the
         # field does not have access to the initial value
         return self.initial["password"]
+
+
+# class RegisterForm(UserCreationForm):
+#     # email = forms.EmailField(required=True)
+#     password = forms.CharField(widget=forms.PasswordInput)
+#     password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+
+
+#     class Meta:
+#         model = CustomUser
+#         fields = ('email',)#, 'password1', 'password2')
+
+#     def save(self, commit=True):
+#         user = super(RegisterForm, self).save(commit=False)
+#         def save(self, commit=True):
+#             user = super(RegisterForm, self).save(commit=False)
+#             user.email = self.cleaned_data['email']
+#             if commit:
+#                 user.save()
+#             return user
+
+
+# class RegisterForm(forms.ModelForm):
+#     password = forms.CharField(widget=forms.PasswordInput)
+#     password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+
+#     class Meta:
+#         model = CustomUser
+#         fields = ('email',)
+
+#     def clean_email(self):
+#         email = self.cleaned_data.get('email')
+#         qs = CustomUser.objects.filter(email=email)
+#         if qs.exists():
+#             raise forms.ValidationError("email is taken")
+#         return email
+
+#     def clean_password2(self):
+#         # Check that the two password entries match
+#         password1 = self.cleaned_data.get("password1")
+#         password2 = self.cleaned_data.get("password2")
+#         if password1 and password2 and password1 != password2:
+#             raise forms.ValidationError("Passwords! don't match")
+#         return password2
+#     def save(self, commit=True):
+#         # Save the provided password in hashed format
+#         user = super(RegisterForm, self).save(commit=False)
+#         user.set_password(self.cleaned_data["password2"])
+#         if commit:
+#             user.save()
+#         return user
