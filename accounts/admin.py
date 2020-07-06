4436a917a9715c06867a81694c72b57e6e908b72
Melvin-Thomas-Dev <57299818+Melvin-Thomas-Dev@users.noreply.github.com>
2020-07-06 11:39:12 +0530

    Added profile inline

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,14 +4,14 @@ from django.contrib.auth.admin import UserAdmin
 from .models import Account
 from profiles.models import ParticipantProfile
 
-# class ParticipantProfileInline(admin.StackedInline):
-#     model = ParticipantProfile
-#     can_delete = False
-#     verbose_name_plural = 'Participant Profile'
-#     fk_name = 'user'
+class ParticipantProfileInline(admin.StackedInline):
+    model = ParticipantProfile
+    can_delete = False
+    verbose_name_plural = 'Participant Profile'
+    fk_name = 'user'
 
 class AccountAdmin(UserAdmin):
-    # inlines = (ParticipantProfileInline, )
+    inlines = (ParticipantProfileInline, )
     list_display = ('email', 'username', 'date_joined', 'last_login', 'is_admin', 'is_staff')
     search_fields = ('email', 'username',)
     readonly_fields = ('date_joined', 'last_login')
@@ -20,9 +20,10 @@ class AccountAdmin(UserAdmin):
     list_filter = ()
     fieldsets = ()
 
-    # def get_inline_instances(self, request, obj=None):
-    #     if not obj:
-    #         return list()
-    #     return super(AccountAdmin, self).get_inline_instances(request, obj)
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(AccountAdmin, self).get_inline_instances(request, obj)
+
 
 admin.site.register(Account, AccountAdmin)
