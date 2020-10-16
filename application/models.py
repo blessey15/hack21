0013b9d520715c94e728610aa1d8dc80a4631d8e
Melvin Thomas <melvinchooranolil@gmail.com>
2020-10-16 14:50:21 +0530

    added virtualenv and updated requirements

--- application/models.py
+++ application/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import Account
+
+APPLICATION_STATUS_CHOICES = (
+    (1,'Submitted'),
+    (2,'Declined'),
+    (3,'Waitinglist'),
+    (4,'Accepted'),
+)
+class Team(models.Model):
+    name = models.CharField(max_length=64, blank=False)
+    admin = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='team')
+    # strength = models.IntegerField(default=1)
+    application_status = models.IntegerField(choices=APPLICATION_STATUS_CHOICES)
