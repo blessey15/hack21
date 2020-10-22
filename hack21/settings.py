4389ee42de126757b67852e9b77204a6ff3f9a11
Melvin Thomas <melvinchooranolil@gmail.com>
2020-10-22 07:59:19 +0530

    develop static files ok

--- hack21/settings.py
+++ hack21/settings.py
@@ -159,12 +159,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 #Static files config
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
+# STATICFILES_DIRS = [
+# os.path.join(BASE_DIR, 'staticfiles'),
+# ]
+
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'static'),
 ]
-
 
 
 AUTH_USER_MODEL='accounts.Account' #custom user model
