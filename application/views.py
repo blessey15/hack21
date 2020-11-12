a8ff74377e184ef0826cc383643071d366caf792
Melvin Thomas <melvinchooranolil@gmail.com>
2020-11-12 14:53:26 +0530

    joining team final

--- application/views.py
+++ application/views.py
@@ -13,11 +13,13 @@ def team_detail_view(request, team_id):
         application = Application.objects.get(team=team)
         # print("has team")
         context['application'] = application
-        try:
-            join_request = JoinRequest.objects.get(team=team, user=request.user)
-            context['sent_request'] = True
-        except JoinRequest.DoesNotExist:
-            context['sent_request'] = False
+        if (request.user in application.members.all()):
+            context['in_team'] = True 
+        # try:
+        #     join_request = JoinRequest.objects.get(team=team, user=request.user)
+        #     context['sent_request'] = True
+        # except JoinRequest.DoesNotExist:
+        #     context['sent_request'] = False
         # print(team.application_team.team.name)
     except Application.DoesNotExist:
         pass
@@ -31,7 +33,9 @@ def join_team_view(request, team_id):
     try:
         application = Application.objects.get(team=team)
         context['application'] = application
-        if not (request.user in application.members.all()):
+        if (request.user in application.members.all()):
+            context['in_team'] = True
+        else:
             application.members.add(request.user)
     #     try:
     #         join_request = JoinRequest.objects.get(team=team, user=request.user)
