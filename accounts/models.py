5135a9b12e963907292f7f6171e2182bc470afcb
Melvin Thomas <melvinchooranolil@gmail.com>
2020-11-18 12:18:16 +0530

    added judge and organizer status to users

--- accounts/models.py
+++ accounts/models.py
@@ -25,7 +25,7 @@ class AccountManager(BaseUserManager):
             username = username,
         )
 
-        user.is_admin = True 
+        user.is_admin = True
         user.is_staff = True
         user.is_superuser = True
         user.save(using = self._db)
@@ -43,7 +43,8 @@ class Account(AbstractBaseUser):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    # is_organizer = models.BooleanField(default=False)
+    is_organizer = models.BooleanField(default=False)
+    is_judge = models.BooleanField(default=False)
     # has_profile = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
