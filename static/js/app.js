ab20c5f0859640157231877d2fea5a06e8680b0e
roshanrajeev <roshanrajeev2001@gmail.com>
2020-11-27 17:50:56 +0530

    Navigation Collapse Fixed

--- static/js/app.js
+++ static/js/app.js
@@ -131,12 +131,27 @@ const navCloseIcon = navIcons.querySelector('.close-icon')
 
 navIcons.addEventListener('click', e=>{
   if(navMenuIcon.classList.contains('menu-active')){
-    navMenuIcon.classList.remove('menu-active')
-    navList.classList.add('nav-list-active')
-    navCloseIcon.classList.add('menu-active')
+    openNavMenu();
   }else{
-    navCloseIcon.classList.remove('menu-active')
-    navMenuIcon.classList.add('menu-active')
-    navList.classList.remove('nav-list-active')
+    closeNavMenu();
   }
 })
+
+document.body.addEventListener('click', e=>{
+  if(!e.target.classList.contains('nav__list') && !e.target.classList.contains('menu-icon'))
+    closeNavMenu();
+})
+
+window.addEventListener('scroll', closeNavMenu);
+
+function closeNavMenu(){
+  navCloseIcon.classList.remove('menu-active')
+  navMenuIcon.classList.add('menu-active')
+  navList.classList.remove('nav-list-active')
+}
+
+function openNavMenu(){
+  navMenuIcon.classList.remove('menu-active')
+  navList.classList.add('nav-list-active')
+  navCloseIcon.classList.add('menu-active')
+}
