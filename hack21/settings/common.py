668e4d2bcb51564ac959d538a26829ff01d6d705
Melvin Thomas <melvinchooranolil@gmail.com>
2020-12-09 12:51:19 +0530

    enthokkeyo cheyyunnu

--- hack21/settings/common.py
+++ hack21/settings/common.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from hack21.auth_pipeline import check_email_exists
+# from hack21.auth_pipeline import check_email_exists
 # import user
 
 
@@ -254,7 +254,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.associate_by_email',
 
     #Custom mail check Pipeline
-    'check_email_exists'
+    # 'check_email_exists'
 
     # Checks if the current social-account is already associated in the site.
     'social_core.pipeline.social_auth.social_user',
