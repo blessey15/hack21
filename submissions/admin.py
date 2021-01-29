7d20da4da04b069bd93b0605a49ecb4f141dcce4
Melvin Thomas <melvinchooranolil@gmail.com>
2021-01-30 04:15:08 +0530

    submission admin

--- submissions/admin.py
+++ submissions/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
-from .models import Abstract
+from .models import Abstract, Submission
 
 class AbstractAdmin(admin.ModelAdmin):
+    list_display = ('get_team',)
+    search_fields = ('get_team',)
+    # list_filter = ('get_team_size',)
+    readonly_fields = ('date_submitted', 'last_modified')
+
+class SubmissionAdmin(admin.ModelAdmin):
     list_display = ('get_team', 'problem_statement', 'project_title', 'get_team_size')
     search_fields = ('get_team', 'project_title')
     # list_filter = ('get_team_size',)
